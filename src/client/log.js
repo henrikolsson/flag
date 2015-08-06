ea64a3e54277177ad45a956ff06d62e2223e6549
Henrik Olsson <henrik@fixme.se>
2015-08-06 16:45:04 +0200

    Rewrite logging

--- src/client/log.js
+++ src/client/log.js
@@ -1,52 +1,13 @@
 var utils = require('./utils');
-
-var maxLines = 10;
 var logNode;
-var lines = [];
-
-function update() {
-  var s = "";
-  for (var i=0;i<lines.length;i++) {
-    var l = lines[i];
-    if (l === null) {
-      l = " ";
-    }
-    if (s.length > 0) {
-      s = s + "\n";
-    }
-    s = s + l;
-  }
-
-  logNode.nodeValue = s;
-}
-
-function init() {
-    logNode = utils.getTextNode(".log");
-    lines = [];
-    for (var i=0;i<maxLines;i++) {
-      lines.push(null);
-    }
-}
 
 module.exports.write = function(line) {
   if (!logNode) {
-    init();
+    logNode = utils.getTextNode(".log");
   }
-
-  if (line.length > 35) {
-    line = line.substring(0, 32) + "...";
+  if (logNode.nodeValue.length > 0) {
+    logNode.nodeValue = logNode.nodeValue + "\n";
   }
-  var i;
-  for (i=0;i<lines.length;i++) {
-    if (lines[i] === null) {
-      lines[i] = line;
-      break;
-    }
-  }
-  if (i === lines.length) {
-    lines.shift();
-    lines.push(line);
-  }
-
-  update();
+  logNode.nodeValue = logNode.nodeValue + line;
+  logNode.parentElement.scrollTop = logNode.parentElement.scrollHeight;
 };
